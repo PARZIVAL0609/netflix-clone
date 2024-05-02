771e825a6173d0a8fa7bbc9f008f3bb00d391cf7
PARZIVAL0609 <dhruvshah6903@gmail.com>
2024-05-02 23:14:07 +0530

    Cleanup, added banner div

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import Row from "./Row";
-import requests from "./requests";
+import requests from "./Requests";
+import Banner from "./Banner";
 
 function App() {
   return (
     <div className="App">
+      {/* Navbar */}
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
