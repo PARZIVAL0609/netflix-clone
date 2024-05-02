7706fd9f8d9b9873855b781279c7512b98f26eca
PARZIVAL0609 <dhruvshah6903@gmail.com>
2024-05-02 22:40:41 +0530

    Added movie row images gave some styling

--- src/Row.js
+++ src/Row.js
@@ -1,30 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import axios from './axios'
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import "./Row.css";
+
+const baseUrl = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-    //A snippet of code that runs based on specific conditions
-    useEffect(()=>{
+  //A snippet of code that runs based on specific conditions
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      console.log(request);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-        async function fetchData(){
-            const request = await axios.get(fetchUrl);
-            console.log(request);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
+  console.table(movies);
 
-    }, []);
-  
-    return (
-        <div>
-            <h2>{title}</h2>
+  return (
+    <div className="row">
+      <h2>{title}</h2>
 
-            {/* container --> posters */}
-
-        </div>
-    )
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className="row_poster"
+            src={`${baseUrl}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Row
+export default Row;
