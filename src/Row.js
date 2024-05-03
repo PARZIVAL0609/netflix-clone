c0b89a0de434fc362f3e46a282779519db0413d1
PARZIVAL0609 <dhruvshah6903@gmail.com>
2024-05-03 23:26:14 +0530

    added trailers (optional also, not working TwT)

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const baseUrl = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   //A snippet of code that runs based on specific conditions
   useEffect(() => {
@@ -18,7 +21,31 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
 
-  // console.table(movies);
+  const opts = {
+    height: "399",
+    width: "100%",
+    playerVars: {
+      //https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    }
+  }
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('');
+    } else {
+      movieTrailer(movie?.name || movie?.title || movie?.original_name ||"")
+        .then(url => {
+          if (url) {
+            const urlParams = new URLSearchParams(new URL(url).search);
+            setTrailerUrl(urlParams.get("v"));
+          } else {
+            console.log("No trailer URL found");
+          }
+        }).catch((error) => console.log(error));
+    }
+  }
+  
 
   return (
     <div className="row">
@@ -28,6 +55,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={()=> handleClick(movie)}
             className={`row_poster ${isLargeRow && "row_posterLarge"}`}
             src={`${baseUrl}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -36,6 +64,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId = {trailerUrl} opt = {opts}/>}
     </div>
   );
 }
